3546af24945a80ab9c01002c2215f40553adcf1f
yunee <yunee@cleaninglab.co.kr>
2024-08-28 11:47:49 +0900

    add merge.sh script

--- merge.sh
+++ merge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Git 설정
-git config user.name "yunee"
-git config user.email "yunee@cleaninglab.co.kr"
+git config user.name "jeon9825"
+git config user.email "jy201735031@gmail.com"
 
 # 모든 브랜치 가져오기
 git fetch --all --unshallow
